68b7d5f4e3abcb7ce76c2b74c8ff4683b1d6aa1c
Evgeny <94135768+iamzimin@users.noreply.github.com>
2023-01-02 21:53:14 +0400

    add join/delete animation

--- Assets/Scripts/CreateLobby/PlayerManager.cs
+++ Assets/Scripts/CreateLobby/PlayerManager.cs
@@ -1,19 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public struct Player
+{
+    public bool isConnected;
+    public string nickname;
+}
+
 public class PlayerManager : MonoBehaviour
 {
-    int test;
+    public Player[] players = new Player[4];
+    public GameObject[] playerObjects = new GameObject[4];
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        players[0].isConnected = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].isConnected)
+            {
+                playerObjects[i].GetComponent<Animator>().SetBool("Open", true);
+
+            }
+            else
+                playerObjects[i].GetComponent<Animator>().SetBool("Open", false);
+        }
     }
 }
