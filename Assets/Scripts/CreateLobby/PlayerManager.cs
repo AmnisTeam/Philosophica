add5f062042d8db2e4e569506c9edd11199f96e0
Evgeny <94135768+iamzimin@users.noreply.github.com>
2023-01-02 22:27:41 +0400

    add kick players on button

--- Assets/Scripts/CreateLobby/PlayerManager.cs
+++ Assets/Scripts/CreateLobby/PlayerManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [Serializable]
 public struct Player
@@ -14,6 +15,7 @@ public class PlayerManager : MonoBehaviour
 {
     public Player[] players = new Player[4];
     public GameObject[] playerObjects = new GameObject[4];
+    public Button button;
     
     // Start is called before the first frame update
     void Start()
@@ -26,6 +28,7 @@ public class PlayerManager : MonoBehaviour
     {
         for (int i = 0; i < players.Length; i++)
         {
+
             if (players[i].isConnected)
             {
                 playerObjects[i].GetComponent<Animator>().SetBool("Open", true);
@@ -34,5 +37,22 @@ public class PlayerManager : MonoBehaviour
             else
                 playerObjects[i].GetComponent<Animator>().SetBool("Open", false);
         }
+    }
+
+    public void KickPlayer_1()
+    {
+        players[0].isConnected = false;
+    }
+    public void KickPlayer_2()
+    {
+        players[1].isConnected = false;
+    }
+    public void KickPlayer_3()
+    {
+        players[2].isConnected = false;
+    }
+    public void KickPlayer_4()
+    {
+        players[3].isConnected = false;
     }
 }
