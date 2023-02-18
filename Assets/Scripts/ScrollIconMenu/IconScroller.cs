8742d54d40238acc0e60dbabff229ee39ddcda29
KoTeuKaSeeker <emaillit8@gmail.com>
2023-02-18 10:09:27 +0400

    Finish work on icon slider

--- Assets/Scripts/ScrollIconMenu/IconScroller.cs
+++ Assets/Scripts/ScrollIconMenu/IconScroller.cs
@@ -10,8 +10,17 @@ public class IconScroller : MonoBehaviour
     public Image[] icons;
     public float offset = 20;
     public Vector2 dragPoint;
-    public float value = 0;
-    public float b;
+    public float value;
+    public float valueVelocity = 0;
+    public float friction = 0.01f;
+    public float gravityVelCof = 0;
+    public int selectedId;
+    public IconsContent iconsContent;
+
+    public Vector2 GetMousePosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
 
     public bool Intersect()
     {
@@ -24,13 +33,48 @@ public class IconScroller : MonoBehaviour
                 (mousePosition.y >= dragFieldPos.y && mousePosition.y <= dragFieldPos.y + rect.height / 100f);
     }
 
+    public float getDelta()
+    {
+        Vector2 delta = Input.mousePosition.ToXY() - oldMousePos;
+        return -delta.x / 100;
+    }
+
+    public void setIconGravity()
+    {
+        float iconWidth = icons[0].GetComponent<RectTransform>().rect.width;
+        float width = (iconWidth + offset) * icons.Length;
+        float minDistance = 99999;
+        int minId = 0;
+
+        for(int x = 0; x < icons.Length; x++)
+        {
+            float distance = Mathf.Abs(icons[x].transform.localPosition.x / (width / 2));
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                minId = x;
+            }
+        }
+
+        valueVelocity = Mathf.Lerp(valueVelocity, -icons[minId].transform.localPosition.x / (width / 2) * gravityVelCof, gravityVelCof);
+    }
+
+    public void updateVelocity()
+    {
+        value += valueVelocity * Time.deltaTime * 100;
+        valueVelocity *= (1 - friction);
+        setIconGravity();
+    }
+
     void Start()
     {
         oldMousePos = Input.mousePosition;
+        value = iconsContent.icons.Length * 1000;
     }
 
     void Update()
     {
+        updateVelocity();
         if (Input.GetMouseButtonDown(0) && Intersect())
         {
             isDrag = true;
@@ -39,30 +83,32 @@ public class IconScroller : MonoBehaviour
         if (isDrag && Input.GetMouseButtonUp(0))
         {
             isDrag = false;
-            Vector2 delta = Input.mousePosition.ToXY() - oldMousePos;
+            valueVelocity = getDelta();
         }
 
         if(isDrag)
         {
-            
+            value += getDelta();
+            valueVelocity = 0;
         }
 
         
         for(int x = 0; x < icons.Length; x++)
         {
-            //float iconWidth = icons[x].GetComponent<RectTransform>().rect.width;
-            //float dragFieldWidth = dragField.GetComponent<RectTransform>().rect.width;
-            //float teleport = (iconWidth + offset) * icons.Length * (int)((value + icons.Length - x) / icons.Length);
-            //float width = (iconWidth + offset) * icons.Length;
-            //icons[x].transform.localPosition = new Vector2(-(x - value) * (iconWidth + offset) + dragFieldWidth / 2 - iconWidth / 2 - teleport, 0);
-            //float posX = icons[x].transform.localPosition.x;
-            //CanvasGroup canvasGroup = icons[x].GetComponent<CanvasGroup>();
-            //float a = ((float)posX / ((iconWidth + offset) * (icons.Length - 2)));
-            //a = 1 - Mathf.Abs(a);
-            //canvasGroup.alpha = a;
-
-
+            float iconWidth = icons[x].GetComponent<RectTransform>().rect.width;
+            float dragFieldWidth = dragField.GetComponent<RectTransform>().rect.width;
+            float teleport = (iconWidth + offset) * icons.Length * (int)((value + icons.Length - x) / icons.Length);
+            float width = (iconWidth + offset) * icons.Length;
+            icons[x].transform.localPosition = new Vector2(-(x - value) * (iconWidth + offset) + dragFieldWidth / 2 - iconWidth / 2 - teleport, 0);
+            int id = x + (int)((value + icons.Length - x) / icons.Length) * icons.Length;
+            icons[x].GetComponent<Image>().sprite = iconsContent.icons[id % iconsContent.icons.Length].sprite;
+            float posX = icons[x].transform.localPosition.x;
+            CanvasGroup canvasGroup = icons[x].GetComponent<CanvasGroup>();
+            float a = ((float)posX / ((iconWidth + offset) * (icons.Length - 2)));
+            a = 1 - Mathf.Abs(a);
+            canvasGroup.alpha = a;
         }
+        selectedId = ((int)(Mathf.Round(value)) + 1) % iconsContent.icons.Length;
 
         oldMousePos = Input.mousePosition;
     }
